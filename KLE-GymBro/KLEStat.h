936005c3fcfd4a19d9916668977085ac4af70e62
neuromanxr <kelv17@gmail.com>
2014-11-07 18:48:58 -0500

    started implementing core data..

--- KLE-GymBro/KLEStat.h
+++ KLE-GymBro/KLEStat.h
@@ -8,18 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+// User picked exercise. User picks goal sets, reps and weight
+
 @class KLEExercises;
 
 @interface KLEStat : NSObject
 
 @property (nonatomic, strong) KLEExercises *exerciseObject;
-@property (nonatomic, retain) NSString *exercise;
+@property (nonatomic, retain) NSString *exercise; // should be using KLEExercise Object
 @property (nonatomic, retain) NSDate *date;
 @property (nonatomic) int sets;
 @property (nonatomic) int reps;
 @property (nonatomic) float weight;
 
-@property (nonatomic, copy) NSIndexPath *userSelections;
+//@property (nonatomic, copy) NSIndexPath *userSelections;
 
 - (instancetype)initWithExercise:(NSString *)exerciseName
                     numberOfSets:(int)setsNumber
